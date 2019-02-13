a1ef620f599f0f1430d6b17f0e7027490a179ba4
unknown <parikhdarshan36@gmail.com>
2019-02-14 00:07:46 +0530

    Added components
    Signed-off-by: unknown <parikhdarshan36@gmail.com>

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+    serverId: number = 10;
+    private serverStatus: string = 'online';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = 'No server created!';
+  serverName = 'Test server';
+  userName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement> event.target).value;
+  }
+
+  ngOnInit() {
+  }
+
+  onEmptyUserField() {
+    this.userName = '';
+  }
+
+}
